d083b5cc74c86b43f600fde6e172324846b40e5d
GwenaelWest <48352232+GwenaelWest@users.noreply.github.com>
2020-05-26 16:12:54 +0200

    Maj BD Gwen

--- sql/databaseStartScript.sql
+++ sql/databaseStartScript.sql
@@ -1,5 +1,5 @@
 ﻿-- --------------------------------------------------------
--- Host:                         127.0.0.1
+-- Host: ProjetWebVege                        127.0.0.1
 -- Server version:               10.4.11-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
 -- HeidiSQL Version:             10.2.0.5599
@@ -13,40 +13,38 @@
 
 
 -- Dumping database structure for snows
-DROP DATABASE IF EXISTS `snows`;
-CREATE DATABASE IF NOT EXISTS `snows` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
-USE `snows`;
+DROP DATABASE IF EXISTS `vege`;
+CREATE DATABASE IF NOT EXISTS `vege` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `vege`;
 
 -- Dumping structure for table snows.snows
-DROP TABLE IF EXISTS `snows`;
-CREATE TABLE IF NOT EXISTS `snows` (
+DROP TABLE IF EXISTS `vege`;
+CREATE TABLE IF NOT EXISTS `vege` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `code` varchar(4) NOT NULL,
-  `brand` varchar(20) NOT NULL,
-  `model` varchar(30) NOT NULL,
-  `snowLength` int(4) unsigned NOT NULL,
-  `qtyAvailable` smallint(6) NOT NULL DEFAULT 0,
+  `name` VARCHAR(20) NOT NULL,
+  `origin` varchar(20) NOT NULL,
+  `variety` VARCHAR(20) NULL,
+  `color` VARCHAR(20) NOT NULL,
   `description` varchar(200) NOT NULL DEFAULT '0',
-  `dailyPrice` float unsigned NOT NULL,
   `photo` varchar(50) DEFAULT NULL,
-  `active` tinyint(4) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `snow_code` (`code`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- Dumping data for table snows.snows: ~9 rows (approximately)
-DELETE FROM `snows`;
+DELETE FROM `vege`;
 /*!40000 ALTER TABLE `snows` DISABLE KEYS */;
-INSERT INTO `snows` (`id`, `code`, `brand`, `model`, `snowLength`, `qtyAvailable`, `description`, `dailyPrice`, `photo`, `active`) VALUES
-	(1, 'B101', 'Burton', 'Custom', 160, 22, 'La board la plus fiable de tous les temps, la solution snowboard pour tous les terrains. (Homme)', 29, 'view/content/images/B101_small.jpg', 1),
-	(2, 'B126', 'Burton', 'Free Thinker', 165, 2, 'Élargissez votre vision grâce son interprétation du ride tout terrain dynamique sur la poudreuse. (Homme)', 45, 'view/content/images/B126_small.jpg', 1),
-	(3, 'B327', 'Burton', 'Day Trader', 155, 6, 'Flottabilité sans effort et un contrôle qui renforce la confiance en soi. (Femme)', 25, 'view/content/images/B327_small.jpg', 0),
-	(4, 'K266', 'K2', 'Wildheart', 152, 2, 'Keeping in versatile style (Femme)', 29, 'view/content/images/K266_small.jpg', 1),
-	(5, 'N100', 'Nidecker', 'Tracer', 164, 11, 'Une expérience de carve hors du commun. Idéal pour carver comme jamais (Homme et femme)', 39, 'view/content/images/N100_small.jpg', 1),
-	(6, 'N754', 'Nidecker', 'Ultralight', 166, 26, 'A la pointe de la technologie. Idéal pour le freeride sur les faces engagées (Homme et femme)', 59, 'view/content/images/N754_small.jpg', 1),
-	(7, 'P067', 'Prior', 'Brandwine 153', 154, 9, 'High performance, directional Freeride board, draws a smooth, stable and fast line through all snow conditions. (Femme)', 49, 'view/content/images/P067_small.jpg', 1),
-	(8, 'P165', 'Prior', 'BC Split 161', 169, 1, 'Sa forme directionnelle Freeride offre une ride plutôt douce et stable dans une variété de conditions', 35, 'view/content/images/P165_small.jpg', 1),
-	(9, 'K409', 'K2', 'Lime Lite', 149, 15, 'Best For Freestyle Evolution with a Focus on Fun (Femme)', 55, 'view/content/images/K409_small.jpg', 1);
+INSERT INTO `vege` (`id`, `name`, `origin`, `variety`, `color`, `description`, `photo`) VALUES
+	(1, 'Banane','Brésil', NULL, 'Jaune', 'Banane exotique venant du Brésil cultivé avec pesticide.', '' ),
+	(2, 'Fraise','Espagne', NULL, 'Rouge', 'Fraises provenant d'' espagne cultivé en pleine airs avec pesticide. ', '' ),
+	(3, 'Pomme','Suisse', 'Golden', 'Jaune', 'Pommes Golden Bio cultivé en Suisse avec le respect des normes écologique.', '' ),
+	(4, 'Pomme','France', 'La Gala', 'Rouge', 'Pommes Gala juteuse et sucrée.', '' ),
+	(5, 'Orange','Tunisie', NULL, 'Orange', 'Oranges bio . ', '' ),
+	(6, 'Salade','Suisse', 'Boston', 'Vert', 'Avec ses superbes feuilles soyeuses, la laitue Boston est délicieuse.', '' ),
+	(7, 'Carotte','Portugal', NULL, 'Orange', 'Belle carottes provenant du Portugal, Culture Bio.', '' ),
+	(8, 'Pomme de terre','Suisse', NULL, 'Jaune', 'Pomme de terre Suisse, avec toute la qualité que cela implique.', '' ),
+	(9, 'Champignons','Nouvelle-Zélande ', NULL, 'Brun', 'Champignons des bois.', '' ),
+	(10, 'Brocoli','Canada', NULL, 'Vert', 'Brocoli canadien, et tout le monde sait que le canada c est cool.', '' );
+	
 /*!40000 ALTER TABLE `snows` ENABLE KEYS */;
 
 -- Dumping structure for table snows.users
