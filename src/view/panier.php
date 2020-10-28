3805baf9c262e5b88e308dad06ff5e798f05c5aa
Christnovie <48351212+Christnovie@users.noreply.github.com>
2020-10-28 04:38:49 +0100

    no article

--- src/view/panier.php
+++ src/view/panier.php
@@ -15,7 +15,8 @@ $title = "Panier";
         <div class="container">
             <div class="row no-gutters slider-text align-items-center justify-content-center">
                 <div class="col-md-9 ftco-animate text-center">
-                    <p class="breadcrumbs"><span class="mr-2"><a href="view/content/index.html">Home</a></span> <span>Cart</span></p>
+                    <p class="breadcrumbs"><span class="mr-2"><a href="view/content/index.html">Home</a></span> <span>Cart</span>
+                    </p>
                     <h1 class="mb-0 bread">My Cart</h1>
                 </div>
             </div>
@@ -39,30 +40,48 @@ $title = "Panier";
                             </tr>
                             </thead>
                             <tbody>
-                            <?php $total =0 ;$discount = 0; foreach (  $_SESSION['panier']->getListItem() as $item) :?>
-                            <tr class="text-center">
-                                <td class="product-remove"><a href="index.php?action=remove&add=<?= $item[0]->element ?>"><span class="ion-ios-close"></span></a></td>
 
-                                <td class="image-prod"><div class="img" style="background-image:url(<?=$item[0]->photo; ?>);"></div></td>
+                            <?php
+                            $total = 0;
+                            $discount = 0;
+                            if ($_SESSION['panier']->getListItem() == 0) :?>
+                                <tr class="text-center">
+                                    <td colspan="12" class="contents align-content-center ">
+                                        <a href="index.php?action=displayArticles" class="btn btn-primary align-content-center"> Vous avez aucun article cliquer pour en ajouter</a>
+                                    </td>
+                                </tr>
+                            <?php else: ?>
+                                <?php foreach ($_SESSION['panier']->getListItem() as $item) : ?>
+                                    <tr class="text-center">
+                                        <td class="product-remove"><a
+                                                    href="index.php?action=remove&add=<?= $item[0]->element ?>"><span
+                                                        class="ion-ios-close"></span></a></td>
 
-                                <td class="product-name">
-                                    <h3><?=$item[0]->element ?> </h3>
-                                    <p><?=$item[0]->description ?> </p>
-                                </td>
+                                        <td class="image-prod">
+                                            <div class="img"
+                                                 style="background-image:url(<?= $item[0]->photo; ?>);"></div>
+                                        </td>
 
-                                <td class="price"><?=$item[0]->price ?> </td>
+                                        <td class="product-name">
+                                            <h3><?= $item[0]->element ?> </h3>
+                                            <p><?= $item[0]->description ?> </p>
+                                        </td>
 
-                                <td class="quantity">
-                                    <div class="input-group mb-3">
-                                        <input type="text" name="quantity" class="quantity form-control input-number" value="<?=$item[0]->quantity ?> " min="1" max="100">
-                                    </div>
-                                </td>
+                                        <td class="price"><?= $item[0]->price ?> </td>
 
-                                <td class="total"><?php if(  round($item[0]->price * $item[0]->quantity,2 )%1 > 0)echo round($item[0]->price * $item[0]->quantity,2 )."CHF" ;else echo$item[0]->price * $item[0]->quantity.".-CHf";  ?></td>
-                            </tr><!-- END TR-->
-                            <?php $total += $item[0]->price * $item[0]->quantity;
-                            endforeach; ?>
+                                        <td class="quantity">
+                                            <div class="input-group mb-3">
+                                                <input type="text" name="quantity"
+                                                       class="quantity form-control input-number"
+                                                       value="<?= $item[0]->quantity ?> " min="1" max="100">
+                                            </div>
+                                        </td>
 
+                                        <td class="total"><?php if (round($item[0]->price * $item[0]->quantity, 2) % 1 > 0) echo round($item[0]->price * $item[0]->quantity, 2) . "CHF"; else echo $item[0]->price * $item[0]->quantity . ".-CHf"; ?></td>
+                                    </tr><!-- END TR-->
+                                    <?php $total += $item[0]->price * $item[0]->quantity;
+                                endforeach; ?>
+                            <?php endif; ?>
                             </tbody>
                         </table>
                     </div>
@@ -108,20 +127,20 @@ $title = "Panier";
                         <h3>Cart Totals</h3>
                         <p class="d-flex">
                             <span>Subtotal</span>
-                            <span><?php if(  fmod(round($total,2 ),1) > 0)echo round($total,2 )."CHF" ;else echo $total.".-CHf";  ?></span>
+                            <span><?php if (fmod(round($total, 2), 1) > 0) echo round($total, 2) . "CHF"; else echo $total . ".-CHf"; ?></span>
                         </p>
-                       <p class="d-flex">
+                        <p class="d-flex">
                             <span>Delivery</span>
-                            <span><?=3.95 % 1 ?></span>
+                            <span><?= 3.95 % 1 ?></span>
                         </p>
                         <p class="d-flex">
                             <span>Discount</span>
-                            <span><?php if(  fmod( round($discount,2 ),1) > 0)echo round($discount,2 )."CHF" ;else echo $discount.".-CHf";  ?></span>
+                            <span><?php if (fmod(round($discount, 2), 1) > 0) echo round($discount, 2) . "CHF"; else echo $discount . ".-CHf"; ?></span>
                         </p>
                         <hr>
                         <p class="d-flex total-price">
                             <span>Total</span>
-                            <span><?php if(  fmod(round($total-$discount,2 ),1) > 0)echo round($total-$discount,2 )."CHF" ;else echo $total-$discount.".-CHf";  ?></span>
+                            <span><?php if (fmod(round($total - $discount, 2), 1) > 0) echo round($total - $discount, 2) . "CHF"; else echo $total - $discount . ".-CHf"; ?></span>
                         </p>
                     </div>
                     <p><a href="index.php?action=checkout" class="btn btn-primary py-3 px-4">Proceed to Checkout</a></p>
